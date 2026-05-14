e427c330255a78683895e897a4182c56eec73dfd
23071sh <23071sh@hvhs.school.nz>
2026-05-15 09:52:48 +1200

    15/05

--- script.js
+++ script.js
@@ -126,9 +126,38 @@ function fb_sortHighScores(snapshot){
   HTML_OUTPUT.innerHTML = "";
   snapshot.forEach(function(child){
     console.log(child.key + " got " + child.val() + " points");
-    HTML_OUTPUT.innerHTML += child.key + " : " + child.val();
+    HTML_OUTPUT.innerHTML +=
+    "<p>" + child.key + " : " + child.val() + " points</p>";
   });
 }
 function fb_showOnceScore(child){
   console.log(child.key+" got "+ child.val()+" points");
 }
+
+var GLOBAL_user; //Google's user object
+
+//Set up a listener for the login state of the user
+function fb_login(){
+   authenticationListener = firebase.auth().onAuthStateChanged(fb_handleLogin);
+}
+// Run when the login state of the user changes. 
+function fb_handleLogin(_user){
+    if (_user){
+        console.log("User is logged in")
+        GLOBAL_user = _user; //Save the user details object to a global variable
+ }  else {
+    console.log("user is Not logged in - Starting the popup process")
+    fb_popupLogin();
+ }
+}
+
+//Run the google login popup
+function fb_popupLogin(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then((result) => {
+        GLOBAL_user = result.user; //Save the user object to a global variable
+        console.log("User has logged in")
+    });
+}
+
